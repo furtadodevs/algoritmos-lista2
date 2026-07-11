d9a2129cb61fae09e9907019da66c6b149c53d1a
Gabrielle Furtado <gabriellerodriguesfurtado@gmail.com>
2026-07-10 21:41:49 -0300

    Atividade 3

---
+++ atividade03.php
@@ -0,0 +1,54 @@
+<?php
+/*Atividade 3 - Consumo de combustível
+Uma pessoa realizou uma viagem de carro. Armazene e calcule
+● a distância percorrida.
+● a quantidade de combustível consumida.
+● Calcule o consumo médio: consumo médio = distância ÷ combustível
+● Exiba a distância, o combustível gasto e o consumo médio.
+● Se o consumo médio for maior ou igual a 12 km/l, exiba: O veículo teve um bom
+consumo.*/
+
+// Entradas - Variáveis
+$kilometro = 60;
+$combustivel = 5;
+$consumo = "";
+
+// Processamento
+$consumoMedio = $kilometro / $combustivel;
+
+//Se o consumo médio for maior ou igual a 12 km/l, exiba: O veículo teve um bom consumo
+if ($consumoMedio >= 12) {
+    $consumo = "Bom consumo";
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IF</title>
+</head>
+
+<body>
+<h2>Atividade 3</h2>
+    <br>
+    <p>Uma pessoa realizou uma viagem de carro. Armazene e calcule
+● a distância percorrida.
+● a quantidade de combustível consumida.
+● Calcule o consumo médio: consumo médio = distância ÷ combustível
+● Exiba a distância, o combustível gasto e o consumo médio.
+● Se o consumo médio for maior ou igual a 12 km/l, exiba: O veículo teve um bom
+consumo.
+</p>
+
+    <p><?php echo ("Kilomêtro: " . $kilometro); ?></p>
+    <p><?php echo ("Combustível: " . $combustivel); ?> </p>
+    <p><?php echo ("Consumo: " . $consumo); ?></p>
+    <p><?php echo ("Consumo médio: " . $consumoMedio); ?></p>
+</body>
+
+</html>
