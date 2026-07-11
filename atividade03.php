16849d9df5ab3ddf6951d216d3d5dd0876c9931e
Gabrielle Furtado <gabriellerodriguesfurtado@gmail.com>
2026-07-10 21:42:40 -0300

    Atualização - Atividade 03

--- atividade03.php
+++ atividade03.php
@@ -34,16 +34,16 @@ if ($consumoMedio >= 12) {
 </head>
 
 <body>
-<h2>Atividade 3</h2>
+    <h2>Atividade 3</h2>
     <br>
     <p>Uma pessoa realizou uma viagem de carro. Armazene e calcule
-● a distância percorrida.
-● a quantidade de combustível consumida.
-● Calcule o consumo médio: consumo médio = distância ÷ combustível
-● Exiba a distância, o combustível gasto e o consumo médio.
-● Se o consumo médio for maior ou igual a 12 km/l, exiba: O veículo teve um bom
-consumo.
-</p>
+        ● a distância percorrida.
+        ● a quantidade de combustível consumida.
+        ● Calcule o consumo médio: consumo médio = distância ÷ combustível
+        ● Exiba a distância, o combustível gasto e o consumo médio.
+        ● Se o consumo médio for maior ou igual a 12 km/l, exiba: O veículo teve um bom
+        consumo.
+    </p>
 
     <p><?php echo ("Kilomêtro: " . $kilometro); ?></p>
     <p><?php echo ("Combustível: " . $combustivel); ?> </p>
